38de6f242448d90621bd3676041f62433cdedbea
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2024-11-18 12:20:15 +0100

    add .nojekyll

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'docs',
-  basePath: '/damianzamolski.github.io',
-  assetPrefix: '/damianzamolski.github.io/',
 };
 
 export default nextConfig;
