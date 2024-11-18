6c04ce01aad5c39a4b2eb2a76e8cda44cb9d022d
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2024-11-18 12:13:51 +0100

    fix asset prefix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'docs',
-  assetPrefix: './',
+  basePath: '/DamianZamolski.github.io',
+  assetPrefix: '/DamianZamolski.github.io/',
 };
 
 export default nextConfig;
